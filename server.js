546b815e23f9ef1ce355c86567bfc0d2c795f2e9
vinhb2014719 <145516008+vinhb2014719@users.noreply.github.com>
2023-11-01 06:59:51 +0700

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ async function startServer() {
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}.`)
         });
-    } catch (error) {
+    } catch (err) {
         console.log("Cannot connect to the database!", error);
         process.exit();
     }
